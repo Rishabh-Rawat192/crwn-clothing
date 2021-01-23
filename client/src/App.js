38f086c202eb043322bc34588378f562d901f132
Rishabh Rawat <rishabhrawat192@gmail.com>
2021-01-23 11:33:47 +0530

    Added mobile responsiveness to the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
