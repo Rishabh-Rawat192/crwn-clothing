ed6e1a09667411ee9bfbbc24d8d5873601587541
Rishabh Rawat <rishabhrawat192@gmail.com>
2021-01-24 16:48:08 +0530

    bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import * as serviceWorker from "./service-worker";
+import * as registerServiceWorker from "./serviceWorkerRegistration.js";
 
 import "./index.css";
 import App from "./App";
@@ -23,4 +23,4 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-serviceWorker.register();
+registerServiceWorker.register();
